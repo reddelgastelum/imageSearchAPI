ed14f5f5f99262a9846b8d898490adbecfd9f5e5
System <none>
2017-10-03 03:35:05 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 // init project
 var express = require('express');
-var Search = require('bing.search')
+var GoogleSearch = require('google-search');
 var app = express();
 
 // we've started you off with Express, 
@@ -14,14 +14,22 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  var search = new Search('8637b13076894935811ddeeacd29a9a1');
-  
-  search.web('Tutta Bella Neapolitan Pizzeria',{top: 5},function(err, results) {
-    if (err) {
-      response.send(err);
-    }
-    response.send(results);
+  var googleSearch = new GoogleSearch({
+    key:'AIzaSyCApkMK7aXTu2sICjTbvugeHeEpvC4fMfA',
+    cx:'005615984503445160763:mimerm0isbe'
   });
+  
+  googleSearch.build({
+    q: "tom",
+    start: 5,
+    fileType: "pdf",
+    gl: "tr", //geolocation, 
+    lr: "lang_tr",
+    num: 10, // Number of search results to return between 1 and 10, inclusive 
+    siteSearch: "" // Restricts results to URLs from a specified site 
+}, function(error, response) {
+  console.log(response);
+});
 });
 
 // Simple in-memory store for now
